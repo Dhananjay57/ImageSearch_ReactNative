b8141db56cf3c14596b5fd878baa2dbc1c459653
dhananjay <dhananjayims057@gmail.com>
2019-01-05 21:30:32 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {
+  createStackNavigator,
+  createAppContainer,
+} from 'react-navigation';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import SearchPage from './src/SearchPage'
+import SearchResults from './src/SearchResults'
+
+const AppNavigator = createStackNavigator({
+  Home: { screen: SearchPage },
+  Results:{ screen: SearchResults}
+});
+export default createAppContainer(AppNavigator);
+
+
+
+
+
