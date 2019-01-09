d9dac34ee055dc5d1b1358a580c8170473de3895
dhananjay <dhananjayims057@gmail.com>
2019-01-09 18:44:40 +0530

    FlatList with default column 2

--- App.js
+++ App.js
@@ -8,6 +8,14 @@ import {
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import SearchPage from './src/SearchPage'
 
+// type Props = {};
+// export default class App extends Component<Props> {
+//   render() {
+//     return (
+//       <SearchPage />
+//     );
+//   }
+// }
 const AppNavigator = createStackNavigator({
   Home: { screen: SearchPage }
 });
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -11,9 +11,11 @@ import {
   Image,
   TouchableHighlight,
   FlatList,
-} from 'react-native';
-
+  } from 'react-native';
+  const pageNumber = 1
+  const numColumns = 3
 class ListItem extends React.PureComponent {
+    
     _onPress = () => {
         this.props.onPressItem(this.props.index);
       }
@@ -59,12 +61,16 @@ function urlForQueryAndPage(key, value, pageNumber) {
         searchString: 'london',
         isLoading: false,
         dataSource: null,
+        searchNumber:'2',
         message: '',
       };
     }
   
     _onSearchTextChanged = (event) => {
       this.setState({ searchString: event.nativeEvent.text });
+    };
+    _onSearchNumberOfColumnChanged = (event) =>{
+        this.setState({searchNumber: event.nativeEvent.text});
     };
   
     _executeQuery = (query) => {
@@ -80,15 +86,16 @@ function urlForQueryAndPage(key, value, pageNumber) {
     };
   
     _onSearchPressed = () => {
-      const query = urlForQueryAndPage('q', encodeURIComponent(this.state.searchString), 1);
+      const query = urlForQueryAndPage('q', encodeURIComponent(this.state.searchString), pageNumber);
       this._executeQuery(query);
+      this.setState({numColumns: this.state.searchNumber})
     };
   
     _handleResponse = (response) => {
       this.setState({ isLoading: false , message: '' });
       if (response) {
           this.setState({
-              dataSource:response, isLoading:true
+              dataSource:response, isLoading:false
             
           })
         // this.props.navigation.navigate(
@@ -105,6 +112,17 @@ function urlForQueryAndPage(key, value, pageNumber) {
         onPressItem={this._onPressItem}
         />
       );
+      onEndReached = () => {
+        if (!this.onEndReachedCalledDuringMomentum) {
+          console.log('onEndReached()', this.state.dataSource)
+          let dataSource = this.state.dataSource
+          this._onSearchPressed('q', encodeURIComponent(this.state.searchString), pageNumber+1)
+          let newData = dataSource.concat(this.dataSource)
+          this.setState({dataSource: newData})
+          console.log('newData##', newData)
+          this.onEndReachedCalledDuringMomentum = true;
+        }
+      }
       _onPressItem = (index) => {
         console.log("Pressed row: "+index);
       }
@@ -127,6 +145,13 @@ function urlForQueryAndPage(key, value, pageNumber) {
               value={this.state.searchString}
               onChange={this._onSearchTextChanged}
               placeholder='Search images'/>
+              <TextInput
+              underlineColorAndroid= {'transparent'}
+              style={styles.searchInput}
+              value={this.state.searchNumber}
+              onChange={this._onSearchNumberOfColumnChanged}
+              placeholder='number'/>
+
             <Button
               onPress={this._onSearchPressed}
               color='#48BBEC'
@@ -136,9 +161,13 @@ function urlForQueryAndPage(key, value, pageNumber) {
           {spinner}
           <Text style={styles.description}>{this.state.message}</Text>
            <FlatList
+          numColumns={2}
           data={this.state.dataSource}
           keyExtractor={this._keyExtractor}
           renderItem={this._renderItem}
+         // onEndReached={this.onEndReached}
+          onEndReachedThreshold={0.5}
+          onMomentumScrollBegin={() => { this.onEndReachedCalledDuringMomentum = false }}
         />
         </View>
         
@@ -183,8 +212,8 @@ function urlForQueryAndPage(key, value, pageNumber) {
         padding: 5
       },
       thumb: {
-        width: 300,
-        height: 100,
+        width: 100,
+        height:100,
         marginRight: 10
       },
   });
